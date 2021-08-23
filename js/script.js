61f3a5d0124136ab5ade3e35bc3ad179e56db8d7
pedrohemmel <phenrique_h2016@hotmail.com>
2021-08-23 17:44:58 -0300

    seção de equipe criadora concluida faltando apenas alocar as fotos

--- js/script.js
+++ js/script.js
@@ -1,15 +1,40 @@
 let todosProdutos = document.getElementById('todosProdutos');
+let acessoRedes = document.getElementById('acessoRedes');
+let footer = document.getElementById('footer');
+let apresentacaoCeo = document.getElementById('apresentacaoCeo');
+let contato = document.getElementById('contato');
+let maisVendidos = document.getElementById('maisVendidos');
 
 function apareceDiv() {
     if(todosProdutos.style.display == 'block') {
         todosProdutos.style.display = 'none';
+        footer.style.display = 'block';
+        apresentacaoCeo.style.display = 'block';
+        contato.style.display = 'block';
+        maisVendidos.style.display = 'block';
     } else {
         todosProdutos.style.display = 'block';
+        footer.style.display = 'none';
+        apresentacaoCeo.style.display = 'none';
+        contato.style.display = 'none';
+        maisVendidos.style.display = 'none';
     }
 }
 
 function voltaMaisVendidos() {
     if(todosProdutos.style.display == 'block') {
         todosProdutos.style.display = 'none';
+        footer.style.display = 'block';
+        apresentacaoCeo.style.display = 'block';
+        contato.style.display = 'block';
+        maisVendidos.style.display = 'block';
     } 
+}
+
+function apareceRedes() {
+    if(acessoRedes.style.display == 'block') {
+        acessoRedes.style.display = 'none';
+    } else {
+        acessoRedes.style.display = 'block';
+    }
 }

015f8427c4e922c3ba0131f16700f0da971a055a
pedrohemmel <phenrique_h2016@hotmail.com>
2021-08-25 15:33:07 -0300

    footer pronto -- estrutura do web site pronto, faltando apenas alterar fotos

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 let todosProdutos = document.getElementById('todosProdutos');
 let acessoRedes = document.getElementById('acessoRedes');
+let navbar = document.getElementById('navbar');
+let bottomNavbar = document.getElementById('bottom-navbar-container');
+let banners = document.getElementById('banners-container');
+let apresentacao = document.getElementById('apresentacao');
 let footer = document.getElementById('footer');
 let apresentacaoCeo = document.getElementById('apresentacaoCeo');
 let contato = document.getElementById('contato');
 let maisVendidos = document.getElementById('maisVendidos');
+let sejaParceiro = document.getElementById('sejaParceiro');
 
 function apareceDiv() {
     if(todosProdutos.style.display == 'block') {
@@ -12,24 +17,26 @@ function apareceDiv() {
         apresentacaoCeo.style.display = 'block';
         contato.style.display = 'block';
         maisVendidos.style.display = 'block';
+        sejaParceiro.style.display = 'block';
+        apresentacao.style.display = 'block';
+        bottomNavbar.style.display = 'block';
+        banners.style.display = 'block';
+        navbar.style.display = 'block';
     } else {
         todosProdutos.style.display = 'block';
         footer.style.display = 'none';
         apresentacaoCeo.style.display = 'none';
         contato.style.display = 'none';
         maisVendidos.style.display = 'none';
+        sejaParceiro.style.display = 'none';
+        apresentacao.style.display = 'none';
+        bottomNavbar.style.display = 'none';
+        banners.style.display = 'none';
+        navbar.style.display = 'none';
     }
 }
 
-function voltaMaisVendidos() {
-    if(todosProdutos.style.display == 'block') {
-        todosProdutos.style.display = 'none';
-        footer.style.display = 'block';
-        apresentacaoCeo.style.display = 'block';
-        contato.style.display = 'block';
-        maisVendidos.style.display = 'block';
-    } 
-}
+
 
 function apareceRedes() {
     if(acessoRedes.style.display == 'block') {

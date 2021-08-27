b7b060c27f83a3679ce89084f35c310894b3169d
pedrohemmel <phenrique_h2016@hotmail.com>
2021-08-27 18:05:25 -0300

    Alterações solicitadas concluida

--- js/script.js
+++ js/script.js
@@ -10,24 +10,6 @@ let contato = document.getElementById('contato');
 let maisVendidos = document.getElementById('maisVendidos');
 let sejaParceiro = document.getElementById('sejaParceiro');
 
-function apareceDiv() {
-    if(todosProdutos.style.display == 'block') {
-        location.reload(true);
-    } else {
-        todosProdutos.style.display = 'block';
-        footer.style.display = 'none';
-        apresentacaoCeo.style.display = 'none';
-        contato.style.display = 'none';
-        maisVendidos.style.display = 'none';
-        sejaParceiro.style.display = 'none';
-        apresentacao.style.display = 'none';
-        bottomNavbar.style.display = 'none';
-        banners.style.display = 'none';
-        navbar.style.display = 'none';
-    }
-}
-
-
 
 function apareceRedes() {
     if(acessoRedes.style.display == 'block') {
